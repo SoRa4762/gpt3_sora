144c19df2bc6d7e55ca6a7d8b2a43550e262b0d0
来 SoRa <sonamraisx007@gmail.com>
2023-03-06 17:21:41 +0545

    initital commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { CTA, Navbar, Brand } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Possibility,
+  Header,
+  WhatGPT3,
+} from "./containers";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
